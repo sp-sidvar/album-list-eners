3fdc0bc6ee2f57669d78f62989dc4e165ab70f8e
sp-sidvar <gce.jorge@gmail.com>
2026-08-31 18:05:47 -0600

    C:/Program Files/Git/Gallery - Se genera cambio de estado para siempre mostrar albumes distintos por medio de aleatoriedad

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,13 +1,38 @@
 import { Album } from "./Album.jsx";
 import data from "../data/db.json";
 import "../classes/gallery-style.css";
+import { useState } from 'react';
+
+const shuffleArray = (array) => {
+  const copy = [...array];
+  for (let i = copy.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [copy[i], copy[j]] = [copy[j], copy[i]];
+  }
+  return copy;
+};
 
 export function Gallery() {
+  const [randomizedData] = useState(() => shuffleArray(data));
+
   return (
     <section className="content">
-      {data.map((album) => (
+      {randomizedData.map((album) => (
         <Album key={album.id} data={album} />
       ))}
     </section>
   );
 }
+
+
+
+
+// export function Gallery() {
+//   return (
+//     <section className="content">
+//       {data.map((album) => (
+//         <Album key={album.id} data={album} />
+//       ))}
+//     </section>
+//   );
+// }
